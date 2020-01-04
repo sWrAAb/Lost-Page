b1ae84c3d435fe585b2b57ae6a61d121c2ab8f21
sWrAAb <swraab@gmail.com>
2020-01-04 23:29:49 +0000

    tested if mongodb is conected and connected two navbar links

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 from os import path
 
 if path.exists("env.py"):
@@ -9,11 +10,17 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'TheLostPage'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+mongo = PyMongo(app)
 
 @app.route('/')
+
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
 @app.route('/books')
 def books():
-    return render_template("books.html")
+    return render_template("books.html", books=mongo.db.books.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
