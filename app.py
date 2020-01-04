2a1225766e16d3dd565efc382c6110f1537557c9
sWrAAb <swraab@gmail.com>
2020-01-04 22:13:40 +0000

    added books.html with accordion, and connected app.py to mongoDB

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask, render_template
+from os import path
 
-
-
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'TheLostPage'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 
 @app.route('/')
-def index():
-    return render_template("base.html")
+@app.route('/books')
+def books():
+    return render_template("books.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
