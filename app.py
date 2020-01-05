5756d97631b1da543e6258de4c3bca76bed1c9c0
sWrAAb <swraab@gmail.com>
2020-01-05 18:56:49 +0000

    created addbook.html, routed it, added a form to it and styled it a bit

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
-
 @app.route('/home')
 def index():
     return render_template("index.html")
@@ -21,6 +20,14 @@ def index():
 @app.route('/books')
 def books():
     return render_template("books.html", books=mongo.db.books.find())
+
+@app.route('/categories')
+def categories():
+    return render_template("categories.html", books=mongo.db.categories.find())
+
+@app.route('/add_book')
+def add_book():
+    return render_template("addbook.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
