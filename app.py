18d29d07ab6bc54da0b2dce34cca8bd6cc551d08
sWrAAb <swraab@gmail.com>
2020-01-18 20:20:08 +0000

    added buttons for categories  and shop

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+import datetime
+from flask import Flask, redirect, render_template, request, url_for, jsonify, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -21,7 +22,27 @@ def index():
 
 @app.route('/books')
 def books():
-    return render_template('books.html', books=mongo.db.books.find())
+    adventure = mongo.db.books.find( { 'genre':' Adventure'} )
+    comedy = mongo.db.books.find( { 'genre':' Comedy'} )
+    divination = mongo.db.books.find( { 'genre':'Divination'} )
+    fantasy = mongo.db.books.find( { 'genre':'Fantasy'} )
+    fiction = mongo.db.books.find( { 'genre':'Fiction'} )
+    mystery = mongo.db.books.find( { 'genre':'Mystery'} )
+    nonfiction = mongo.db.books.find( { 'genre':'Nonfiction'} )
+    romance = mongo.db.books.find( { 'genre':'Romance'} )
+    
+    return render_template('books.html', 
+                            books=mongo.db.books.find(), 
+                            categories=mongo.db.categories.find(), 
+                            adventure=adventure, 
+                            comedy=comedy,
+                            divination=divination,
+                            fantasy=fantasy,
+                            fiction=fiction,
+                            mystery=mystery,
+                            nonfiction=nonfiction,
+                            romance=romance)
+                                                        
 
 @app.route('/categories')
 def categories():
@@ -78,6 +99,11 @@ def update_book(book_id):
 def delete_book(book_id):
     mongo.db.books.remove({'_id': ObjectId(book_id)})
     return redirect(url_for('books'))
+
+@app.context_processor
+def books_total():
+    books_count = mongo.db.books.count
+    return dict(books_count=books_count)
 
 
 if __name__ == "__main__":
