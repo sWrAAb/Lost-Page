7c7e35c16cc5169c11d221353a84d335587f85fd
sWrAAb <swraab@gmail.com>
2020-01-19 18:07:47 +0000

    added modal to share a book link, added home image and icon on navbar, some overall style and checked all files with online validators

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-import datetime
-from flask import Flask, redirect, render_template, request, url_for, jsonify, session
+from flask import Flask, redirect, render_template, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -9,7 +8,12 @@ import re
 if path.exists("env.py"):
     import env
 
+# create instance of flask and assign it to app
+
 app = Flask(__name__)
+
+# MongoDB URI / Assign database
+
 app.config["MONGO_DBNAME"] = 'TheLostPage'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
@@ -21,15 +25,16 @@ mongo = PyMongo(app)
 def index():
     return render_template('index.html', books=mongo.db.books.find())
 
+# ----- READ ALL ----- #
+
 @app.route('/books')
 def books():
     """
     Read all books from database.
 
     Display all books initially, with option to Search.
-    Search function works by reading url args for existing data.
+    Search function works by reading args for existing data.
     """
-    # show correct authors on cards
     
     return render_template('books.html', 
                             books=mongo.db.books.find(), 
@@ -56,7 +61,10 @@ def categories():
     
     return render_template('categories.html', books = mongo.db.books.find())
 
-# Add a book to database #
+
+#    CRUD: Create | Read | Update | Delete    #
+
+# --Create-- #
 
 @app.route('/add_book')
 def add_book():
@@ -64,27 +72,45 @@ def add_book():
 
 @app.route('/insert_book', methods=['POST'])
 def insert_book():
+    """
+    Create book in database.
+
+    Inject all form data to new book document on submit.
+    """
     books = mongo.db.books
     books.insert_one(request.form.to_dict())
     return redirect(url_for('books'))
 
-# View book info #
+
+# ----- READ ONE ----- #
 
 @app.route('/view_book/<book_id>', methods=["GET", "POST"])    
 def view_book(book_id):
+    """
+    Read book in database.
+
+    Gather details from document for displaying to user.
+    """
     the_book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     all_books = mongo.db.books.find({"_id": ObjectId(book_id)})
     return render_template("viewbook.html", books = all_books, book=the_book)
 
-# Edit book #
+# ----- UPDATE ----- #
 
 @app.route('/edit_book/<book_id>')
 def edit_book(book_id):
+    """
+    Update book in database.
+
+    Inject all existing data from the book back into the form.
+    
+    Bug alert. Description text area returns empty.
+    """
     the_book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     all_categories =  mongo.db.categories.find()
     return render_template('editbook.html', book=the_book, categories=all_categories)
 
-# Update form #
+#----- UPDATE FORM ----- #
 
 @app.route('/update_book/<book_id>', methods=["POST"])
 def update_book(book_id):
@@ -100,15 +126,22 @@ def update_book(book_id):
     })
     return redirect(url_for('books'))
 
-# Delete book #
+# ----- DELETE ----- #
 
 @app.route('/delete_book/<book_id>')
 def delete_book(book_id):
+    """
+    Delete book from database.
+    """
     mongo.db.books.remove({'_id': ObjectId(book_id)})
     return redirect(url_for('books'))
 
-@app.context_processor
+@app.context_processor    
 def books_total():
+    """
+    Show total number of books in collection.
+    Displayed in navbar.
+    """
     books_count = mongo.db.books.count
     return dict(books_count=books_count)
 
---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+//Share the Book//
+
+
+/* insert URL into <input> */
+var bookUrl = $(location).attr("href");
+$("#share-btn").on("click", function() {
+    $("#share-url").val(bookUrl);
+});
+/* copy value of <input> */
+$("#copy-btn").on("click", function() {
+    var copyUrl = $("#share-url").val(bookUrl);
+    copyUrl.select();
+    document.execCommand("copy");
+});
